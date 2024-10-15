8fdf986a8f470ca3e0b7341848c2fcb77b94ad42
Ali A.karim Ali <ali-karim99@hotmail.com>
2024-10-15 16:38:31 +0300

    sign/in/up update

--- middleware/index.js
+++ middleware/index.js
@@ -1,11 +1,12 @@
-
 const bcrypt = require('bcrypt')
 const { status } = require('express/lib/response')
 const jwt = require('jsonwebtoken')
+
 require('dotenv').config()
 
 const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
 const APP_SECRET = process.env.APP_SECRET
+
 
 const hashPassword = async (password) => {
     let hashPassword = await bcrypt.hash(password, SALT_ROUNDS)
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Load Dep
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors');
 
 //require and initalize dotenv
 require('dotenv').config()
@@ -8,9 +9,14 @@ require('dotenv').config()
 //PORT conf
 const PORT = process.env.PORT
 
+
 //Initalize express
 const app = express()
 app.use(express.json()); 
+
+// Enable CORS for specific origin
+app.use(cors({ origin: 'http://localhost:5173' })); // Allow requests from the frontend
+
 
 //Database Configuration
 const db = require('./config/db')
