b7e852877c0c47f3fc77a423d7e5b8b0a7497eef
nawraahmed <nawra.alhaji@gmail.com>
2024-10-16 11:18:11 +0300

    changes attraction

--- models/Attraction.js
+++ models/Attraction.js
@@ -2,26 +2,13 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const AttractionSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  imageUrl: {
-    type: String, // URL of the image for the attraction
-    required: false
-  },
-  age: {
-    type: Number,
-    required: true
-  },
-  size: {
-    type: Number,
-    required: true
-  },
-  indoor: {
-    type: Boolean,
-    required: true
-  }
+  name: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  age: { type: Number, required: true },
+  size: { type: Number, required: true },
+  indoor: { type: Boolean, required: true },
+  top: { type: String, required: true },
+  left: { type: String, required: true }
 })
 
 module.exports = mongoose.model('Attraction', AttractionSchema)
